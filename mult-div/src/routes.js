af5c54a00c5b9c9210c2992ea67ba1fb3d0c771a
jamesclaws <leonardusjamesw@gmail.com>
2022-09-29 23:38:58 +0700

    fix: rename account to mult-div, change function and made dockerfile

---
+++ mult-div/src/routes.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const express = require('express');
+const api = express.Router();
+
+api.get('/', (req, res) => {
+  res.send({ message: 'Hello World!' });
+});
+
+api.get('/whoami', (req, res) => {
+  res.status(200).send('mult-div');
+});
+
+api.post('/mult', (req, res) => {
+  res.status(200).send(`${req.body.x * req.body.y}`);
+});
+
+api.post('/div', (req, res) => {
+  res.status(200).send(`${req.body.x / req.body.y}`);
+});
+
+module.exports = api;
