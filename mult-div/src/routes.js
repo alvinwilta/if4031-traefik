76b99d0f71ef199ab5a005a7bb7de66b0040a8fc
alvinwilta <13519163@std.stei.itb.ac.id>
2022-09-30 12:03:21 +0700

    standarize output

--- mult-div/src/routes.js
+++ mult-div/src/routes.js
@@ -4,7 +4,7 @@ const express = require("express");
 const api = express.Router();
 
 api.get("/", (req, res) => {
-  res.send({ message: `Hello World!` });
+  res.send(`Hello World!`);
 });
 
 api.get("/whoami", (req, res) => {
